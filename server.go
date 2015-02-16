6f7ae1bed4663da75f1715d3c2cc5f1c70d5a39c
RDuX <ruben.dens@gmail.com>
2015-02-16 13:51:59 +0100

    Basic gouncer server. Enabled config pass through from main caller and configured the primary endpoints

---
+++ server.go
@@ -0,0 +1,76 @@
+package gouncer
+
+import (
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	Port           string
+	Certificate    string
+	CertificateKey string
+	Cache          []string
+	GroupDB        string
+	UserDB         string
+	Version        string
+	Name           string
+	Usage          string
+}
+
+func NewServer(port string) *Server {
+	return &Server{
+		Port: port,
+	}
+}
+
+func (srv *Server) Start() {
+	// Confiugre CORS
+	corsRules := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"POST", "GET", "HEAD", "OPTIONS"},
+	})
+
+	// Wrap the http handlers in a cors handler
+	corsInfo := corsRules.Handler(http.HandlerFunc(srv.InfoHandler))
+	corsAuthentication := corsRules.Handler(http.HandlerFunc(srv.AuthenticationHandler))
+	corsAuthorization := corsRules.Handler(http.HandlerFunc(srv.AuthorizationHandler))
+
+	// Define routes
+	http.Handle("/", corsInfo)
+	http.Handle("/authenticate", corsAuthentication)
+	http.Handle("/authorize", corsAuthorization)
+
+	// Attempt to start the server. On error server exits with status 1
+	if err := http.ListenAndServeTLS(srv.Port, srv.Certificate, srv.CertificateKey, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Authentication handler checks the method and delegates a token request
+func (srv *Server) AuthenticationHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// Configure the Authenticator instance
+		authenticator := NewAuthenticator(w, r)
+		authenticator.Cache = srv.Cache
+		authenticator.GroupDB = srv.GroupDB
+		authenticator.UserDB = srv.UserDB
+
+		// Execute a token request
+		authenticator.HandleTokenRequest()
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Error 405: Method not allowed\n"))
+	}
+}
+
+func (srv *Server) AuthorizationHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// @TODO make motd configurable
+func (srv *Server) InfoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write([]byte("{\"name\":\"" + srv.Name + "\", \"version\": \"" + srv.Version + "\", \"description\": \"" + srv.Usage + "\", \"message\": \"g(B)ouncer - Check people at the door!\"}"))
+}
