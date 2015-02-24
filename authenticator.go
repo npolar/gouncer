af3d29897edb77f32d2f7fde4ba292f2e326f32b
RDuX <ruben.dens@gmail.com>
2015-02-24 14:54:44 +0100

    First working draft of the authenticator

--- authenticator.go
+++ authenticator.go
@@ -3,6 +3,9 @@ package gouncer
 import (
 	"encoding/base64"
 	"encoding/json"
+	"encoding/xml"
+	"github.com/bradfitz/gomemcache/memcache"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
@@ -18,61 +21,72 @@ type Authenticator struct {
 	Password string
 	Response *AuthResponse
 	Error    *AuthError
-	Writer   http.ResponseWriter
-	Request  *http.Request
 }
 
 type AuthResponse struct {
-	Token string `json:"token,omitempty"`
+	Token string `json:"token,omitempty" xml:"Token"`
 }
 
 type AuthError struct {
-	Status  int    `json:"status,omitempty"`
-	Error   string `json:"error,omitempty"`
-	Message string `json:"message,omitempty"`
+	Status  int    `json:"status,omitempty" xml:"Status,attr"`
+	Error   string `json:"error,omitempty" xml:"Error"`
+	Message string `json:"message,omitempty" xml:"Message"`
 }
 
-func NewAuthenticator(w http.ResponseWriter, r *http.Request) *Authenticator {
+func NewAuthenticator() *Authenticator {
 	return &Authenticator{
-		Writer:  w,
-		Request: r,
+		Response: &AuthResponse{},
+		Error:    &AuthError{},
 	}
 }
 
-func (auth *Authenticator) HandleTokenRequest() {
-	auth.ExtractBasicAuth(auth.Request.Header.Get("Authorization"))
+func (auth *Authenticator) HandleTokenRequest(w http.ResponseWriter, r *http.Request) {
+	auth.ExtractBasicAuth(r.Header.Get("Authorization"))
 	auth.GenerateToken()
 
 	// Check if an error occured. If so return error else return token
-	if auth.Error == nil {
-		auth.TokenResponse()
+	if auth.Error.Error == "" {
+		auth.TokenResponse(w, r)
 	} else {
-		auth.ErrorResponse()
+		auth.ErrorResponse(w, r)
 	}
 }
 
-func (auth *Authenticator) TokenResponse() {
-	switch auth.Request.Header.Get("Accept") {
+func (auth *Authenticator) TokenResponse(w http.ResponseWriter, r *http.Request) {
+	switch r.Header.Get("Accept") {
 	case "application/json":
 		jsonResponse, _ := json.Marshal(auth.Response)
-		auth.Writer.Write(jsonResponse)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(jsonResponse)
+	case "application/xml", "text/xml":
+		xmlResponse, _ := xml.MarshalIndent(auth.Response, "", "  ")
+		xmlResponse = []byte(xml.Header + string(xmlResponse))
+		w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+		w.Write(xmlResponse)
 	default:
-		auth.Writer.Write([]byte(auth.Response.Token))
+		w.Write([]byte(auth.Response.Token))
 	}
 }
 
-func (auth *Authenticator) ErrorResponse() {
-	auth.Writer.WriteHeader(auth.Error.Status)
+func (auth *Authenticator) ErrorResponse(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(auth.Error.Status)
 
-	switch auth.Request.Header.Get("Accept") {
+	switch r.Header.Get("Accept") {
 	case "application/json":
 		jsonResponse, _ := json.Marshal(auth.Error)
-		auth.Writer.Write(jsonResponse)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(jsonResponse)
+	case "application/xml", "text/xml":
+		xmlResponse, _ := xml.MarshalIndent(auth.Error, "", "  ")
+		xmlResponse = []byte(xml.Header + string(xmlResponse))
+		w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+		w.Write(xmlResponse)
 	default:
-		auth.Writer.Write([]byte(auth.Error.String()))
+		w.Write([]byte(auth.Error.String()))
 	}
 }
 
+// @TODO move into the server layer so it can be reused in the authorizer layer
 func (auth *Authenticator) ExtractBasicAuth(authHeader string) {
 	if authHeader != "" {
 		basicAuthRxp := regexp.MustCompile("(?i)Basic\\s(.+)$")
@@ -111,17 +125,51 @@ func (auth *Authenticator) ParseCredentials(basicAuth string) {
 }
 
 func (auth *Authenticator) GenerateToken() {
-	auth.Response = &AuthResponse{
-		Token: "faketokenz...",
+	if tokenizer, err := NewTokenizer(auth.Password, auth.FetchUser()); err == nil {
+		tokenizer.GroupDB = auth.GroupDB
+		token, err := tokenizer.GenerateJWT()
+
+		if err != nil {
+			auth.SetError(http.StatusUnauthorized, "Unauthorized", err.Error())
+		}
+
+		if err = auth.CacheTokenInfo(tokenizer.Secret); err == nil {
+			auth.Response.Token = token
+		} else {
+			auth.SetError(http.StatusInternalServerError, "Internal server error", err.Error())
+		}
+	} else {
+		auth.SetError(http.StatusUnauthorized, "Unautherized", err.Error())
 	}
 }
 
-func (auth *Authenticator) SetError(status int, httpError string, message string) {
-	auth.Error = &AuthError{
-		Status:  status,
-		Error:   httpError,
-		Message: message,
+// CacheTokenInfo caches the username and secret for validation purposes
+func (auth *Authenticator) CacheTokenInfo(secret string) error {
+	mc := memcache.New(auth.Cache...)
+	return mc.Set(&memcache.Item{Key: auth.Username, Value: []byte(secret), Expiration: 1200})
+}
+
+func (auth *Authenticator) FetchUser() map[string]interface{} {
+	var caller = make(map[string]interface{})
+
+	if response, err := http.Get(auth.UserDB + "/" + auth.Username); err == nil {
+		user, _ := ioutil.ReadAll(response.Body)
+		defer response.Body.Close()
+
+		if err = json.Unmarshal(user, &caller); err != nil {
+			auth.SetError(http.StatusInternalServerError, "Internal Server Error", "Error retrieving user info")
+		}
+	} else {
+		auth.SetError(http.StatusInternalServerError, "Internal Server Error", "Error retrieving user info")
 	}
+
+	return caller
+}
+
+func (auth *Authenticator) SetError(status int, httpError string, message string) {
+	auth.Error.Status = status
+	auth.Error.Error = httpError
+	auth.Error.Message = message
 }
 
 func (authErr *AuthError) String() string {
