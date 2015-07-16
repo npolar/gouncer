23bf7d654ccf17b5cd4929878784c37c7b079498
RDuX <ruben.dens@gmail.com>
2015-07-16 13:23:43 +0200

    remove log messages

--- confirm.go
+++ confirm.go
@@ -1,7 +1,6 @@
 package gouncer
 
 import (
-	"log"
 	"net/http"
 	"strings"
 )
@@ -19,15 +18,13 @@ func NewConfirmation(h *ResponseHandler) *Confirmation {
 
 func (c *Confirmation) ConfirmRegistration() {
 	segs := strings.Split(c.Handler.HttpRequest.URL.Path, "/")
-	log.Println(segs[len(segs)-1])
 
 	if item, err := c.Backend.Cache.Get(segs[len(segs)-1]); err == nil {
 		doc := item.Value
 
 		couch := NewCouch(c.Backend.Server, c.Backend.UserDB)
-		if response, err := couch.Post(doc); err == nil {
+		if _, err := couch.Post(doc); err == nil {
 			c.Backend.Cache.Delete(segs[len(segs)-1])
-			log.Println(response)
 			c.Handler.Writer.Write([]byte("Registration successfull. You can now login with your new account."))
 		} else {
 			c.Handler.NewError(http.StatusInternalServerError, err.Error())
