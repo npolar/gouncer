55e0a9462651930f8885c168f613e49b985aa24d
RDuX <ruben.dens@gmail.com>
2015-07-21 12:30:30 +0200

    moved mailing logic into a more generic mail implementation

--- register.go
+++ register.go
@@ -3,21 +3,16 @@ package gouncer
 import (
 	"crypto"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/npolar/toki"
 	"io/ioutil"
-	"log"
-	"net"
 	"net/http"
-	"net/smtp"
 	"regexp"
 )
 
 type Register struct {
 	*Core
-	*Mail
+	*MailConfig
 	Credentials
 	Handler *ResponseHandler
 	RegistrationInfo
@@ -67,9 +62,16 @@ func (r *Register) processRegistration() {
 
 	if err != nil {
 		if err.Error() == "404 Object Not Found" {
-			if cache, err := r.cacheRegistrationRequest(); err == nil {
-				r.sendConfirmationMail(r.resolveHost(), cache)
-				r.Handler.NewResponse(http.StatusOK, "In a few moments you will receive a confirmation email at: "+r.RegistrationInfo.Email+". To complete the registration click the link inside.")
+			if id, err := r.cacheRegistrationRequest(); err == nil {
+				mail := NewMailClient(r.RegistrationInfo.Email, id)
+				mail.MailConfig = r.MailConfig
+				mail.Backend = r.Backend
+				mail.Core = r.Core
+				if err := mail.Confirmation(); err == nil {
+					r.Handler.NewResponse(http.StatusOK, "In a few moments you will receive a confirmation email at: "+r.RegistrationInfo.Email+". To complete the registration click the link inside.")
+				} else {
+					r.Handler.NewError(http.StatusInternalServerError, err.Error())
+				}
 			} else {
 				r.Handler.NewError(http.StatusInternalServerError, err.Error())
 			}
@@ -114,13 +116,20 @@ func (r *Register) AuthorizedToken() {
 			if valid {
 
 				r.Credentials.HashAlg = crypto.SHA1
-				key := r.Credentials.GenerateHash(username + r.TimeSalt() + r.CharSalt(32))
+				id := r.Credentials.GenerateHash(username + r.TimeSalt() + r.CharSalt(32))
 
-				err = r.Backend.Cache.Set(&memcache.Item{Key: key, Value: []byte(username), Expiration: 1200})
+				err = r.Backend.Cache.Set(&memcache.Item{Key: id, Value: []byte(username), Expiration: 1200})
 
 				if err == nil {
-					r.sendCancelationMail(r.resolveHost(), key)
-					r.Handler.NewResponse(http.StatusOK, "In a few moments you will receive a confirmation email at: "+r.Username+". To complete the cancellation click the link inside.")
+					mail := NewMailClient(r.Username, id)
+					mail.MailConfig = r.MailConfig
+					mail.Backend = r.Backend
+					mail.Core = r.Core
+					if err := mail.Cancellation(); err == nil {
+						r.Handler.NewResponse(http.StatusOK, "In a few moments you will receive a confirmation email at: "+r.Username+". To complete the cancellation click the link inside.")
+					} else {
+						r.Handler.NewError(http.StatusInternalServerError, err.Error())
+					}
 				}
 			}
 		}
@@ -129,49 +138,6 @@ func (r *Register) AuthorizedToken() {
 	if err != nil {
 		r.Handler.NewError(http.StatusUnauthorized, err.Error())
 	}
-}
-
-// localIP tries to resolve the local IP of the server
-func (r *Register) localIP() (net.IP, error) {
-	tt, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-	for _, t := range tt {
-		aa, err := t.Addrs()
-		if err != nil {
-			return nil, err
-		}
-		for _, a := range aa {
-			ipnet, ok := a.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			v4 := ipnet.IP.To4()
-			if v4 == nil || v4[0] == 127 { // loopback address
-				continue
-			}
-			return v4, nil
-		}
-	}
-	return nil, errors.New("cannot find local IP address")
-}
-
-func (r *Register) resolveHost() string {
-	// Set host to localhost as default
-	host := "localhost"
-
-	// Check if an external hostname was provided. if so use this instead
-	if r.Core.Hostname != "" {
-		host = r.Core.Hostname
-	} else {
-		// If no hostname is configured use the IP address
-		if ip, err := r.localIP(); err == nil {
-			host = ip.String()
-		}
-	}
-
-	return host
 }
 
 // cacheRegistrationRequest generates a password has for the password in the request and
@@ -228,109 +194,4 @@ func (r *Register) defaultGroups() []string {
 
 	// If none of the group domains matched return the default group
 	return r.Groups["default"].Groups
-}
-
-// sendConfirmationMail generates an smtp request with a confirmation email to the user
-func (r *Register) sendConfirmationMail(host string, confirmationID string) {
-	var message string
-	confirmregxp := regexp.MustCompile("{{confirm}}")
-	link := "https://" + host + r.Core.Port + "/confirm/" + confirmationID
-
-	// Load the mail contents
-
-	if r.Mail.ConfirmMessage != "" {
-		message = "Subject:" + r.Mail.ConfirmSubject + "\n\n"
-		message += confirmregxp.ReplaceAllString(r.Mail.ConfirmMessage, link)
-	} else { // When no message is configured use a generic registration message
-		message = "Subject:Account Registration\n\nThank you for registering.\n\nTo complete your registration please click the following link: " + link + "\n\nIf you did not try to register an account with us feel free to ignore or delete this message."
-	}
-
-	// Connect to the remote SMTP server specified through the commandline.
-	c, err := smtp.Dial(r.Backend.Smtp)
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Set the sender
-	if err := c.Mail(r.Mail.Sender); err != nil {
-		log.Println(err)
-	}
-
-	// Set the recipient
-	if err := c.Rcpt(r.RegistrationInfo.Email); err != nil {
-		log.Println(err)
-	}
-
-	// Send the email body.
-	wc, err := c.Data()
-	if err != nil {
-		log.Println(err)
-	}
-	_, err = fmt.Fprintf(wc, message)
-	if err != nil {
-		log.Println(err)
-	}
-	err = wc.Close()
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Send the QUIT command and close the connection.
-	err = c.Quit()
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func (r *Register) sendCancelationMail(host string, cancellationID string) {
-	var message string
-	confirmregxp := regexp.MustCompile("{{cancel}}")
-	link := "https://" + host + r.Core.Port + "/cancel/" + cancellationID
-
-	// Load the mail contents
-
-	if r.Mail.CancelMessage != "" {
-		message = "Subject:" + r.Mail.CancelSubject + "\n\n"
-		message += confirmregxp.ReplaceAllString(r.Mail.CancelMessage, link)
-	} else { // When no message is configured use a generic registration message
-		message = "Subject:Account Cancellation\n\nTo complete your cancellation request please click the following link: " + link + "\n\nIf you did not try to cancel your account delete this message."
-	}
-
-	// Load the mail contents
-
-	// Connect to the remote SMTP server specified through the commandline.
-	c, err := smtp.Dial(r.Backend.Smtp)
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Set the sender
-	if err := c.Mail(r.Mail.Sender); err != nil {
-		log.Println(err)
-	}
-
-	// Set the recipient
-	if err := c.Rcpt(r.Username); err != nil {
-		log.Println(r.Username, err)
-	}
-
-	// Send the email body.
-	wc, err := c.Data()
-	if err != nil {
-		log.Println(err)
-	}
-	_, err = fmt.Fprintf(wc, message)
-	if err != nil {
-		log.Println(err)
-	}
-	err = wc.Close()
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Send the QUIT command and close the connection.
-	err = c.Quit()
-	if err != nil {
-		log.Println(err)
-	}
 }
