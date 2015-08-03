ff7f5a178cfbd0d8cc0a54a38e6339cbe0020d10
RDuX <ruben.dens@gmail.com>
2015-08-03 13:40:42 +0200

    refactored: split into multiple methods. improved error handling

--- onetime.go
+++ onetime.go
@@ -24,28 +24,49 @@ func (o *OneTime) RequestPassword() {
 	err := DecodeJsonRequest(o.HttpRequest.Body, &info)
 
 	if err == nil {
-		if info.Email != "" {
-			o.Username = info.Email
-			o.HashAlg = crypto.SHA1
-			onetimePass := o.GenerateHash(info.Email + o.CharSalt(128) + o.TimeSalt())
-			err = o.CacheCredentials(info.Email, []byte(onetimePass), 1800)
+		pwd, perr := o.generateOneTimePassword(info.Email)
+		err = perr
 
-			if err == nil {
-				mail := NewMailClient(info.Email, "")
-				mail.Backend = o.Backend
-				mail.MailConfig = o.MailConfig
-				err = mail.OneTimePassword(onetimePass)
-
-				if err == nil {
-					o.NewResponse(http.StatusOK, "You should receive an email with your one time password in a few moments")
-				}
-			}
-		} else {
-			errors.New("No email address provided")
+		if err == nil {
+			err = o.mailPassword(pwd)
 		}
 	}
 
 	if err != nil {
 		o.NewError(http.StatusInternalServerError, err.Error())
 	}
+}
+
+func (o *OneTime) generateOneTimePassword(user string) (string, error) {
+	if user != "" {
+		var pwd string
+		o.Username = user
+		o.HashAlg = crypto.SHA1
+
+		_, err := o.FetchUser()
+
+		if err == nil {
+			pwd = o.GenerateHash(user + o.CharSalt(128) + o.TimeSalt())
+			err = o.CacheCredentials(user, []byte(pwd), 1800)
+		} else {
+			err = errors.New("User Not found")
+		}
+
+		return pwd, err
+	}
+
+	return "", errors.New("No email address provided")
+}
+
+func (o *OneTime) mailPassword(pwd string) error {
+	mail := NewMailClient(o.Username, "")
+	mail.Backend = o.Backend
+	mail.MailConfig = o.MailConfig
+	err := mail.OneTimePassword(pwd)
+
+	if err == nil {
+		o.NewResponse(http.StatusOK, "You should receive an email with your one time password in a few moments")
+	}
+
+	return err
 }
